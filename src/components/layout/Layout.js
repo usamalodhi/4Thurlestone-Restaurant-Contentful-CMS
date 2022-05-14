65ed427b8b8ad736d92385ff63272e10d64fa472
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-14 07:55:22 +0100

    Add Banner and update Layout to conditionally render Banner based on location - styling not done

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Location } from '@reach/router';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import Banner from '../banner/Banner';
 
 const Layout = ({ children }) => {
   return (
     <Flex h='100vh' direction='column'>
       <Header />
+      <Location>
+        {({ location }) => {
+          if (location.pathname !== '/') {
+            return <Banner />;
+          }
+        }}
+      </Location>
       <Box as='main' flex='1' border='2px solid black'>
         {children}
       </Box>
