32a2cee8e588d1d49870a8ae80d4c26890412fd7
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-16 19:42:53 +0100

    Update BannerBox component with conditional BannerImages based on route - verified on About, Blog and Contact pages. More work required

--- src/components/banner/BannerBox.js
+++ src/components/banner/BannerBox.js
@@ -1,16 +1,76 @@
+// location.pathname === '/about';
+// location.pathname === '/menu';
+// location.pathname === '/contact';
+// location.pathname === '/blog';
+
 import React from 'react';
+import { useLocation } from '@reach/router';
+import { useStaticQuery, graphql } from 'gatsby';
+import { getImage } from 'gatsby-plugin-image';
+import { BgImage } from 'gbimage-bridge';
 import { Box, Flex, Center } from '@chakra-ui/react';
 import BannerLabel from './BannerLabel';
 
 const BannerBox = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulAboutPage {
+        nodes {
+          bannerImage {
+            gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          }
+        }
+      }
+      allContentfulContactPage {
+        nodes {
+          bannerImage {
+            gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          }
+        }
+      }
+      allContentfulBlogPage {
+        nodes {
+          bannerImage {
+            gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          }
+        }
+      }
+    }
+  `);
+
+  console.log('here', data);
+
+  const location = useLocation();
+  const aboutBannerImage = getImage(data.allContentfulAboutPage.nodes[0].bannerImage);
+  // const menuBannerImage = getImage(data.allContentfulMenuPage.nodes[0].bannerImage);
+  const contactBannerImage = getImage(data.allContentfulContactPage.nodes[0].bannerImage);
+  const blogBannerImage = getImage(data.allContentfulBlogPage.nodes[0].bannerImage);
+
+  const switchBannerImage = () => {
+    switch (location.pathname) {
+      case '/about':
+        return aboutBannerImage;
+      // case '/menu':
+      //   return menuBannerImage;
+      case '/contact':
+        return contactBannerImage;
+      case '/blog':
+        return blogBannerImage;
+      default:
+        console.log('error - BannerBox Image');
+    }
+  };
+
   return (
-    <Flex color='white'>
-      <Center w='100vw' h='50vh' bg='green.500'>
-        <Box>
-          <BannerLabel />
-        </Box>
-      </Center>
-    </Flex>
+    <BgImage image={switchBannerImage()} alt='test'>
+      <Flex color='white'>
+        <Center w='100vw' h='50vh'>
+          <Box>
+            <BannerLabel />
+          </Box>
+        </Center>
+      </Flex>
+    </BgImage>
   );
 };
 
