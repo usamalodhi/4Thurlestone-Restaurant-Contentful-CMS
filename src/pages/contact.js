1b0646115b5954bd1e39012d82fc480c2281f878
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-19 08:39:00 +0100

    Update Contact.js with embed iframe of location - Google Maps

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,13 @@
 import React from 'react';
-// import { graphql } from 'gatsby';
 import { Box, SimpleGrid } from '@chakra-ui/react';
 
 const ContactPage = () => {
-  // const imageMap = getImage(data.allContentfulContactPage.nodes[0].contactPageImageMap);
   return (
     <>
       <SimpleGrid minChildWidth='400px' spacingX='1em' spacingY='1em' mt='1em'>
         <Box border='2px solid black'>
           <iframe
+            title='google maps embed of 4Thurlestone'
             width='100%'
             height={600}
             src='https://maps.google.com/maps?q=Thurlestone%20Parade,%20Shepperton%20TW17%209AR&t=&z=13&ie=UTF8&iwloc=&output=embed'
@@ -20,17 +19,5 @@ const ContactPage = () => {
     </>
   );
 };
-
-// export const data = graphql`
-//   {
-//     allContentfulContactPage {
-//       nodes {
-//         contactPageImageMap {
-//           gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-//         }
-//       }
-//     }
-//   }
-// `;
 
 export default ContactPage;
