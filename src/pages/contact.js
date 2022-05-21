15c548b7067dafcab40e5a75aa99b79b6aa671cb
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-21 16:02:51 +0100

    Update contact.js with grayscale map and add address next to map as placeholder content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid, Text } from '@chakra-ui/react';
 
 const ContactPage = () => {
   return (
@@ -11,10 +11,19 @@ const ContactPage = () => {
             width='100%'
             height={600}
             src='https://maps.google.com/maps?q=Thurlestone%20Parade,%20Shepperton%20TW17%209AR&t=&z=13&ie=UTF8&iwloc=&output=embed'
+            style={{ WebkitFilter: 'grayscale(100%)', filter: 'grayscale(100%)' }}
           />
         </Box>
-
-        <Box border='2px solid red'></Box>
+        <Box border='2px solid red'>
+          <Text>01932 558886</Text>
+          <Text>ADDRESS</Text>
+          <Text as='address'>
+            <Text>4 THURLESTONE</Text>
+            <Text>4 Thurlestone Parade</Text>
+            <Text>Shepperton</Text>
+            <Text>TW17 9AR</Text>
+          </Text>
+        </Box>
       </SimpleGrid>
     </>
   );
