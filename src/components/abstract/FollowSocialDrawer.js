0af7ec943068a71238c857db0e83e3208ba321b3
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-22 16:27:31 +0100

    Add new component - FollowSocialDrawer - socialLinks to be inserted in the NavDrawer component

---
+++ src/components/abstract/FollowSocialDrawer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, UnorderedList, ListItem, Image } from '@chakra-ui/react';
+import socialMediaIcons from '../../images';
+
+const FollowSocialDrawer = () => {
+  return (
+    <Box height='100%' as='nav' aria-label='Social Media Links'>
+      <UnorderedList
+        display='flex'
+        border='2px solid white'
+        flexDirection='column'
+        alignItems='flex-end'
+        justifyContent='space-evenly'
+        height='100%'
+        p='0px 0px 0px 0px'
+        m='0px 0px 0px 0px'
+        listStyleType='none'
+        listStylePosition='none'
+      >
+        {socialMediaIcons.map(({ id, url, icon, alt }) => (
+          <ListItem key={id}>
+            <a href={url} target='_blank' rel='noopener noreferrer'>
+              <Image src={icon} alt={alt} width='24px' height='24px' />
+            </a>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </Box>
+  );
+};
+
+export default FollowSocialDrawer;
