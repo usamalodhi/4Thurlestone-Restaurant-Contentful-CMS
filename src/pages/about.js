b61da57c2ebd936c2195e7c278f843e02b939cd1
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-23 16:53:10 +0100

    Update about page stylings with constrained images, color, bgcolor - WIP

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { getImage, GatsbyImage } from 'gatsby-plugin-image';
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid, Box, Text, Center, Flex } from '@chakra-ui/react';
 
 const AboutPage = ({ data }) => {
   console.log(data);
@@ -10,31 +10,78 @@ const AboutPage = ({ data }) => {
   const imageMain = getImage(data.allContentfulAboutPage.nodes[0].aboutPageImageMain);
   const imageDessert = getImage(data.allContentfulAboutPage.nodes[0].aboutPageImageDessert);
 
-  const { aboutPageTextOne } = data.allContentfulAboutPage.nodes[0].aboutPageTextOne;
+  // const { aboutPageTextOne } = data.allContentfulAboutPage.nodes[0].aboutPageTextOne;
 
-  const { aboutPageTextTwo } = data.allContentfulAboutPage.nodes[0].aboutPageTextTwo;
+  // const { aboutPageTextTwo } = data.allContentfulAboutPage.nodes[0].aboutPageTextTwo;
 
-  const { aboutPageTextThree } = data.allContentfulAboutPage.nodes[0].aboutPageTextThree;
+  // const { aboutPageTextThree } = data.allContentfulAboutPage.nodes[0].aboutPageTextThree;
 
   return (
     <>
-      <SimpleGrid minChildWidth='300px' spacingX='1em' spacingY='1em' margin='1em 0em'>
-        <Box border='2px solid black'>{aboutPageTextOne}</Box>
-        <Box border='2px solid black'>
-          <GatsbyImage image={imageChef} />
-        </Box>
+      <SimpleGrid minChildWidth='300px' background='#E0E0E0' color='#EAE6E1'>
+        <Center background='#272625'>
+          <Text padding='0px 24px' textAlign='center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ab laudantium
+            corrupti a hic cupiditate, dolorem doloremque explicabo distinctio quidem dolorum nihil,
+            eos nesciunt impedit tenetur veritatis. Molestiae, magni eum? Quos blanditiis expedita
+            mollitia totam, necessitatibus asperiores eos cumque harum similique molestias atque
+            iusto quaerat magni odit sit facere beatae ipsa sapiente voluptatibus dolores assumenda
+            voluptatem provident officia perspiciatis! Aliquam. Officia numquam fugiat tempora a
+          </Text>
+        </Center>
+        <Center>
+          <GatsbyImage
+            style={{ width: '100%', height: '100%' }}
+            imgStyle={{ objectFit: 'fill' }}
+            image={imageChef}
+            alt='test'
+          />
+        </Center>
       </SimpleGrid>
-      <SimpleGrid columns={1} spacingX='1em' spacingY='1em'>
-        <Box border='2px solid black'>
-          <GatsbyImage image={imageMain} />
-        </Box>
-        <Box border='2px solid black'>{aboutPageTextTwo}</Box>
+
+      <SimpleGrid background='#EAE6E1' color='#272625' columns={1}>
+        <Center>
+          <Text padding='0px 24px' textAlign='center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ab laudantium
+            corrupti a hic cupiditate, dolorem doloremque explicabo distinctio quidem dolorum nihil,
+            eos nesciunt impedit tenetur veritatis. Molestiae, magni eum? Quos blanditiis expedita
+            mollitia totam, necessitatibus asperiores eos cumque harum similique molestias atque
+            iusto quaerat magni odit sit facere beatae ipsa sapiente voluptatibus dolores assumenda
+            voluptatem provident officia perspiciatis! Aliquam. Officia numquam fugiat tempora a
+          </Text>
+        </Center>
+        <GatsbyImage image={imageMain} alt='test' />
+        <Center>
+          <Text padding='0px 24px' textAlign='center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ab laudantium
+            corrupti a hic cupiditate, dolorem doloremque explicabo distinctio quidem dolorum nihil,
+            eos nesciunt impedit tenetur veritatis. Molestiae, magni eum? Quos blanditiis expedita
+            mollitia totam, necessitatibus asperiores eos cumque harum similique molestias atque
+            iusto quaerat magni odit sit facere beatae ipsa sapiente voluptatibus dolores assumenda
+            voluptatem provident officia perspiciatis! Aliquam. Officia numquam fugiat tempora a
+          </Text>
+        </Center>
       </SimpleGrid>
-      <SimpleGrid minChildWidth='300px' spacingX='1em' spacingY='1em' margin='1em 0em'>
-        <Box border='2px solid black'>
-          <GatsbyImage image={imageDessert} />
-        </Box>
-        <Box border='2px solid black'>{aboutPageTextThree}</Box>
+
+      <SimpleGrid color='#EAE6E1' minChildWidth='300px'>
+        <Center>
+          <GatsbyImage
+            style={{ width: '100%', height: '100%' }}
+            imgStyle={{ objectFit: 'fill' }}
+            image={imageDessert}
+            alt='test'
+          />
+        </Center>
+        <Center background='#272625'>
+          <Text padding='0px 24px' textAlign='center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ab laudantium
+            corrupti a hic cupiditate, dolorem doloremque explicabo distinctio quidem dolorum nihil,
+            eos nesciunt impedit tenetur veritatis. Molestiae, magni eum? Quos blanditiis expedita
+            mollitia totam, necessitatibus asperiores eos cumque harum similique molestias atque
+            iusto quaerat magni odit sit facere beatae ipsa sapiente voluptatibus dolores assumenda
+            voluptatem provident officia perspiciatis! Aliquam. Officia numquam fugiat tempora a
+          </Text>
+        </Center>
       </SimpleGrid>
     </>
   );
@@ -54,13 +101,31 @@ export const data = graphql`
           aboutPageTextThree
         }
         aboutPageImageChef {
-          gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          gatsbyImageData(
+            layout: CONSTRAINED
+            height: 700
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+            quality: 100
+          )
         }
         aboutPageImageMain {
-          gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          gatsbyImageData(
+            layout: CONSTRAINED
+            height: 700
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+            quality: 100
+          )
         }
         aboutPageImageDessert {
-          gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          gatsbyImageData(
+            layout: CONSTRAINED
+            height: 700
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+            quality: 100
+          )
         }
       }
     }
