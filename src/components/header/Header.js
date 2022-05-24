cbd0248c8f51c50c351614ac74ee58d5b0b98e31
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-24 15:51:58 +0100

    Update header to be sticky

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, useMediaQuery, useStyleConfig } from '@chakra-ui/react';
+import { background, Box, useMediaQuery, useStyleConfig } from '@chakra-ui/react';
 import NavLinks from './NavBar';
 import NavDrawer from './NavDrawer';
 import Logo from './Logo';
@@ -9,7 +9,16 @@ const Header = () => {
 
   const [isLargerThan600] = useMediaQuery('(min-width: 600px)');
   return (
-    <Box as='header' __css={styles}>
+    <Box
+      style={{
+        zIndex: '10',
+        width: '100%',
+        position: 'fixed',
+        top: '0',
+      }}
+      as='header'
+      __css={styles}
+    >
       <Logo />
       {isLargerThan600 ? <NavLinks /> : <NavDrawer />}
     </Box>
