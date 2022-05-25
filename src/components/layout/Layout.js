1f11c535e9253b2d4237d16be1c75d3f60088fad
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-25 09:32:00 +0100

    Update layout with Poppins font-family to apply globally

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
   );
 
   return (
-    <Flex h='100vh' direction='column'>
+    <Flex fontFamily='Poppins' h='100vh' direction='column'>
       <Header />
       {displayBannerBox}
       <Box as='main' flex='1'>
