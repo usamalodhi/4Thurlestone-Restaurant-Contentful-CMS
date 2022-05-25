f667fd6c4b0ef4713215b33735be0dfba4a6fbf5
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-25 09:30:29 +0100

    Update Menu tab stylings

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -20,20 +20,47 @@ const MenuPage = ({ data }) => {
 
   return (
     <>
-      <Tabs isLazy isFitted variant='enclosed'>
-        <TabList mb='1em'>
-          <Tab>Drinks</Tab>
-          <Tab>Daily</Tab>
-          <Tab>Dessert</Tab>
-          <Tab>Evening</Tab>
+      {/* background='#E0E0E0'
+    colorScheme='gray' */}
+      {/* variant='soft-rounded' */}
+      <Tabs isLazy isFitted variant='unstyled'>
+        <TabList p='0px 0px 0px 0px' background='#white' borderBott='2px solid black'>
+          <Tab
+            _selected={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+            _hover={{ color: '#000000', background: '#eae6e1', fontWeight: 'bold' }}
+            _active={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+          >
+            Drinks
+          </Tab>
+          <Tab
+            _selected={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+            _hover={{ color: '#000000', background: '#eae6e1', fontWeight: 'bold' }}
+            _active={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+          >
+            Daily
+          </Tab>
+          <Tab
+            _selected={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+            _hover={{ color: '#000000', background: '#eae6e1', fontWeight: 'bold' }}
+            _active={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+          >
+            Dessert
+          </Tab>
+          <Tab
+            _selected={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+            _hover={{ color: '#000000', background: '#eae6e1', fontWeight: 'bold' }}
+            _active={{ color: '#eae6e1', background: '#000000', fontWeight: 'bold' }}
+          >
+            Evening
+          </Tab>
         </TabList>
 
-        <TabPanels>
-          <TabPanel>
+        <TabPanels p='0px 0px 0px 0px'>
+          <TabPanel p='0px 0px 0px 0px'>
             <SimpleGrid minChildWidth='300px'>
               <List border='2px solid black'>
                 {teaMenu.map(({ name, price }) => (
-                  <ListItem key={name}>
+                  <ListItem mt='16px' key={name}>
                     <p>{name}</p>
                     <p>{`£${parseFloat(price).toFixed(2)}`}</p>
                   </ListItem>
@@ -41,7 +68,7 @@ const MenuPage = ({ data }) => {
               </List>
               <List border='2px solid black'>
                 {coffeeMenu.map(({ name, price }) => (
-                  <ListItem key={name}>
+                  <ListItem mt='16px' key={name}>
                     <p>{name}</p>
                     <p>{`£${parseFloat(price).toFixed(2)}`}</p>
                   </ListItem>
@@ -50,7 +77,7 @@ const MenuPage = ({ data }) => {
             </SimpleGrid>
           </TabPanel>
 
-          <TabPanel>
+          <TabPanel p='0px 0px 0px 0px'>
             <List>
               {dailySpecialsMenu.map(({ name, price }) => (
                 <ListItem key={name}>
@@ -61,7 +88,7 @@ const MenuPage = ({ data }) => {
             </List>
           </TabPanel>
 
-          <TabPanel>
+          <TabPanel p='0px 0px 0px 0px'>
             <List>
               {dessertsMenu.map(({ name, price }) => (
                 <ListItem key={name}>
@@ -72,7 +99,7 @@ const MenuPage = ({ data }) => {
             </List>
           </TabPanel>
 
-          <TabPanel>
+          <TabPanel p='0px 0px 0px 0px'>
             <p>Set Menu</p>
           </TabPanel>
         </TabPanels>
