696b410a67758bb098853268e733a736d3abc776
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-02 16:24:00 +0100

    Add motion on hover on social icons

--- src/components/abstract/FollowSocial.js
+++ src/components/abstract/FollowSocial.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Box, UnorderedList, ListItem, Image } from '@chakra-ui/react';
 import socialMediaIcons from '../../images';
+import { motion } from 'framer-motion';
 
 const FollowSocial = () => {
   return (
     <Box as='nav' aria-label='Social Media Links'>
       <UnorderedList
         display='flex'
-        // alignItems='center'
         justifyContent='space-between'
         w='100%'
         p='0px 0px 0px 0px'
@@ -18,7 +18,15 @@ const FollowSocial = () => {
         {socialMediaIcons.map(({ id, url, icon, alt }) => (
           <ListItem key={id}>
             <a href={url} target='_blank' rel='noopener noreferrer'>
-              <Image src={icon} alt={alt} width='24px' height='24px' />
+              <Image
+                as={motion.img}
+                whileHover={{ y: -5.0, scale: 1.1 }}
+                transition='0.1s linear'
+                src={icon}
+                alt={alt}
+                width='24px'
+                height='24px'
+              />
             </a>
           </ListItem>
         ))}
