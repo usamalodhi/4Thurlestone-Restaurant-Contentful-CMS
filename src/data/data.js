7c7887d07f6b0c4873fe82154b5fb345c024083a
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-11 09:55:29 +0100

    Replace Blog with News

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ const navLinks = [
   { id: 2, to: '/about', label: 'About' },
   { id: 3, to: '/menu', label: 'Menu' },
   { id: 4, to: '/contact', label: 'Contact' },
-  { id: 5, to: '/blog', label: 'Blog' },
+  { id: 5, to: '/news', label: 'News' },
 ];
 
 export default navLinks;
