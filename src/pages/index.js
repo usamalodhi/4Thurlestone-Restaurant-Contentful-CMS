5a64e0df0f382c06c91dda56a8ed06511bb03018
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-11 09:46:06 +0100

    Update index page styles - WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,38 +7,35 @@ import { Box, Text, Flex } from '@chakra-ui/react';
 const IndexPage = ({ data }) => {
   // const styles = useStyleConfig('IndexPageHero');
   // __css={styles}
+
   const { restaurantTitle, heroSloganTwo } = data.allContentfulIndexPage.nodes[0];
   const heroSloganOne = data.allContentfulIndexPage.nodes[0].heroSloganOne;
   const image = getImage(data.allContentfulIndexPage.nodes[0].heroImageIndex);
 
   return (
-    <BgImage style={{ height: '100vh', backgroundImage: 'contain' }} image={image}>
-      <Flex backgroundColor='rgba(0,0,0,0.5)' h='100vh'>
-        <Flex
-          direction='column'
-          border='2px solid white'
-          height='min-content'
-          padding='16px'
-          margin='auto auto'
-        >
-          <Box>
-            <Text as='h2' textStyle='homePageInfo' layerStyle='homePageTitle'>
-              {heroSloganOne}
-            </Text>
-          </Box>
-          <Box>
-            <Text as='h1' textStyle='homePageTitle' layerStyle='homePageTitle'>
-              {restaurantTitle}
-            </Text>
-          </Box>
-          <Box>
-            <Text as='h2' textStyle='homePageInfo' layerStyle='homePageTitle'>
-              {heroSloganTwo}
-            </Text>
-          </Box>
+    <>
+      <BgImage style={{ height: '100vh', backgroundImage: 'contain' }} image={image}>
+        <Flex backgroundColor='rgba(0,0,0,0.5)' h='100vh'>
+          <Flex direction='column' border='2px solid white' height='min-content' padding='16px' margin='auto auto'>
+            <Box>
+              <Text as='h2' textStyle='homePageInfo' layerStyle='homePageTitle'>
+                {heroSloganOne}
+              </Text>
+            </Box>
+            <Box>
+              <Text as='h1' textStyle='homePageTitle' layerStyle='homePageTitle'>
+                {restaurantTitle}
+              </Text>
+            </Box>
+            <Box>
+              <Text as='h2' textStyle='homePageInfo' layerStyle='homePageTitle'>
+                {heroSloganTwo}
+              </Text>
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
-    </BgImage>
+      </BgImage>
+    </>
   );
 };
 
