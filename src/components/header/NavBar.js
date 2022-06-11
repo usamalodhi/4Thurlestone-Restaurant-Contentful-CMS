c82c2856350fe8e68fdbaeeb373d67f73d8663f8
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-11 09:56:42 +0100

    Abstract unorderedlist styling ino theme

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { Box, useMediaQuery, ListItem, UnorderedList, Text } from '@chakra-ui/react';
+import { Box, useMediaQuery, ListItem, UnorderedList, Text, useStyleConfig } from '@chakra-ui/react';
 import { Link } from 'gatsby';
 import navLinks from '../../data/data';
 import { motion } from 'framer-motion';
 
 const NavBar = ({ onClose }) => {
+  const styles = useStyleConfig('NavBar');
+
   const [isLargerThan600] = useMediaQuery('(min-width: 600px)');
   return (
     <Box as='nav'>
-      <UnorderedList
-        display='inline-flex'
-        columnGap='24px'
-        rowGap='24px'
-        flexDirection={isLargerThan600 ? 'row' : 'column'}
-        listStyleType='none'
-      >
+      <UnorderedList flexDirection={isLargerThan600 ? 'row' : 'column'} __css={styles}>
         {navLinks.map(({ id, to, label }) => (
           <ListItem key={id}>
             <Link onClick={onClose} to={to}>
               <Text
                 as={motion.p}
-                whileHover={{ y: -5.0, color: '#FFFFFF', textDecorationLine: 'underline' }}
+                whileHover={{ y: -5.0, color: '#FFFFFF', scale: 1.1 }}
                 transition='0.1s linear'
                 fontSize={{ base: '16px', sm: '20px' }}
                 textAlign='right'
