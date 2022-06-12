df4ec6b9da610fab2debfb6d6bf45063cc3f5716
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-12 18:36:47 +0100

    Update swiper news component - WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,30 @@ import { getImage } from 'gatsby-plugin-image';
 import { BgImage } from 'gbimage-bridge';
 import { Box, Text, Flex } from '@chakra-ui/react';
 
-const IndexPage = ({ data }) => {
-  // const styles = useStyleConfig('IndexPageHero');
-  // __css={styles}
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
 
-  const { restaurantTitle, heroSloganTwo } = data.allContentfulIndexPage.nodes[0];
-  const heroSloganOne = data.allContentfulIndexPage.nodes[0].heroSloganOne;
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '../swipercss/swipercss.css';
+
+const IndexPage = ({ data }) => {
+  const { restaurantTitle, heroSloganTwo, heroSloganOne } = data.allContentfulIndexPage.nodes[0];
+  const newsItems = data.allContentfulNewsItem.nodes;
+
+  console.log('newsItems', newsItems);
+
   const image = getImage(data.allContentfulIndexPage.nodes[0].heroImageIndex);
 
   return (
     <>
       <BgImage style={{ height: '100vh', backgroundImage: 'contain' }} image={image}>
         <Flex backgroundColor='rgba(0,0,0,0.5)' h='100vh'>
-          <Flex direction='column' border='2px solid white' height='min-content' padding='16px' margin='auto auto'>
+          <Flex direction='column' height='min-content' padding='16px' margin='auto auto'>
             <Box>
-              <Text as='h2' textStyle='homePageInfo' layerStyle='homePageTitle'>
+              <Text as='h2' textStyle='BannerBox' layerStyle='homePageTitle'>
                 {heroSloganOne}
               </Text>
             </Box>
@@ -28,13 +37,47 @@ const IndexPage = ({ data }) => {
               </Text>
             </Box>
             <Box>
-              <Text as='h2' textStyle='homePageInfo' layerStyle='homePageTitle'>
+              <Text as='h2' textStyle='BannerBox' layerStyle='homePageTitle'>
                 {heroSloganTwo}
               </Text>
             </Box>
           </Flex>
         </Flex>
       </BgImage>
+      <Box>
+        <Box h='70vh'>
+          <Swiper
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              dynamicBullets: true,
+            }}
+            loop={true}
+            modules={[Autoplay, Pagination]}
+            className='mySwiper'
+          >
+            {newsItems.map((data) => {
+              return (
+                <SwiperSlide key={data.newsItemName}>
+                  <Flex layerStyle='SwiperSlide'>
+                    <Box>
+                      <Text>{data.newsItemName}</Text>
+                      <Text w='70ch'>{data.newsItemDescription}</Text>
+                    </Box>
+                  </Flex>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </Box>
+      </Box>
     </>
   );
 };
@@ -49,6 +92,12 @@ export const data = graphql`
         heroImageIndex {
           gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
         }
+      }
+    }
+    allContentfulNewsItem {
+      nodes {
+        newsItemName
+        newsItemDescription
       }
     }
   }
