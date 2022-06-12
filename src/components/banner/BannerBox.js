cb4f2d8b52e6fa792e8f66d2a65aca9dbaf10c51
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-12 17:44:29 +0100

    Abstracts s

--- src/components/banner/BannerBox.js
+++ src/components/banner/BannerBox.js
@@ -9,13 +9,7 @@ const BannerBox = ({ bannerImage, altText, bannerLabel }) => {
   return (
     <BgImage image={getImage(bannerImage)} alt={altText}>
       <Flex backgroundColor='rgba(0,0,0,0.5)' h='100vh'>
-        <Center
-          as='h1'
-          textStyle='homePageTitle'
-          layerStyle='homePageTitle'
-          h='100vh'
-          margin='0 auto'
-        >
+        <Center as='h1' textStyle='BannerBox' layerStyle='homePageTitle' h='100vh' margin='0 auto'>
           <Box border='2px solid white' height='min-content' padding='16px'>
             {bannerLabel}
           </Box>
