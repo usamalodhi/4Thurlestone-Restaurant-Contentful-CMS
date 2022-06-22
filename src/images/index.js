85ab94ff33a2846c5f299f3ea298e4be42273fbd
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-06-22 14:36:49 +0100

    Update SocialMediaIcons array with links for TripAdvisor and Google Reviews

--- src/images/index.js
+++ src/images/index.js
@@ -7,9 +7,19 @@ import Twitter from './Twitter.svg';
 
 const socialMediaIcons = [
   { id: 1, icon: FaceBook, alt: 'visit our facebook page', url: 'https://google.com' },
-  { id: 2, icon: Google, alt: 'read our Google reviews', url: 'https://google.com' },
+  {
+    id: 2,
+    icon: Google,
+    alt: 'read our Google reviews',
+    url: 'https://www.google.com/maps/place/4+Thurlestone/@51.3951871,-0.4529705,17z/data=!4m7!3m6!1s0x4876752258168c4d:0x3b8beecae7dadc67!8m2!3d51.395187!4d-0.448488!9m1!1b1',
+  },
   { id: 3, icon: Instagram, alt: 'visit our Instagram page', url: 'https://google.com' },
-  { id: 4, icon: TripAdvisor, alt: 'visit our Trip Advisor page', url: 'https://google.com' },
+  {
+    id: 4,
+    icon: TripAdvisor,
+    alt: 'visit our Trip Advisor page',
+    url: 'https://www.tripadvisor.co.uk/Restaurant_Review-g528815-d24110145-Reviews-4_Thurlestone-Shepperton_Surrey_England.html',
+  },
   { id: 5, icon: Twitter, alt: 'visit our Twitter page', url: 'https://google.com' },
 ];
 
